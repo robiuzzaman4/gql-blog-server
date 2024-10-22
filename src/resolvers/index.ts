88a8131dda30e4518368347ae9f4b764870fc60d
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-22 18:22:54 +0600

    feat: write types for creat user payload

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { CreateUserPayload } from "../types";
 
 const prisma = new PrismaClient();
 
 export const resolvers = {
   Query: {},
   Mutation: {
-    createUser: async (parent: any, args: any, context: any) => {
+    createUser: async (parent: any, args: CreateUserPayload, context: any) => {
       return await prisma.user.create({
         data: args,
       });
