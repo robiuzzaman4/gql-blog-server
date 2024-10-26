cb2dc894092c21cbf84ca4853ba5317fec9c606a
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-26 20:45:25 +0600

    reafactor: separate posts mutations from root mutations

--- src/resolvers/mutation/index.ts
+++ src/resolvers/mutation/index.ts
@@ -1,46 +1,10 @@
-import { CreatePostPayload } from "../../types";
-import { Context } from "../..";
 import { authMutations } from "./auth";
+import { postMutations } from "./post";
 
 export const Mutation = {
   // === AUTH MUTATIONS ===
   ...authMutations,
 
-  // === CREATE POST MUTATION ===
-  createPost: async (
-    _parent: any,
-    args: CreatePostPayload,
-    { prisma, userId }: Context
-  ) => {
-    // if userId not found then thorw error
-    if (!userId) {
-      return {
-        message: "Unauthorised Access!",
-        post: null,
-      };
-    }
-
-    // if title or content not found then throw error
-    if (!args?.title || !args?.content) {
-      return {
-        message: "Title & Content is Required!",
-        post: null,
-      };
-    }
-
-    // create new post
-    const newPost = await prisma.post.create({
-      data: {
-        title: args?.title,
-        content: args?.content,
-        authorId: userId,
-      },
-    });
-
-    // return success response
-    return {
-      message: "Post Created!",
-      post: newPost,
-    };
-  },
+  // === POST MUTATIONS ===
+  ...postMutations,
 };
---
+++ src/resolvers/mutation/post.ts
@@ -0,0 +1,42 @@
+import { Context } from "../..";
+import { CreatePostPayload } from "../../types";
+
+export const postMutations = {
+  // === CREATE POST MUTATION ===
+  createPost: async (
+    _parent: any,
+    args: CreatePostPayload,
+    { prisma, userId }: Context
+  ) => {
+    // if userId not found then thorw error
+    if (!userId) {
+      return {
+        message: "Unauthorised Access!",
+        post: null,
+      };
+    }
+
+    // if title or content not found then throw error
+    if (!args?.title || !args?.content) {
+      return {
+        message: "Title & Content is Required!",
+        post: null,
+      };
+    }
+
+    // create new post
+    const newPost = await prisma.post.create({
+      data: {
+        title: args?.title,
+        content: args?.content,
+        authorId: userId,
+      },
+    });
+
+    // return success response
+    return {
+      message: "Post Created!",
+      post: newPost,
+    };
+  },
+};
