30e9dec4fa785e8e71b47e91c65e6ca8b6d39d1a
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-27 21:44:20 +0600

    feat: update post mutation done

--- src/resolvers/mutation/post.ts
+++ src/resolvers/mutation/post.ts
@@ -41,7 +41,68 @@ export const postMutations = {
   },
 
   // === UPDATE POST MUTATION ===
-  updatePost: async (parent: any, args: any, { prisma }: Context) => {
-    console.log("update-post", args);
+  updatePost: async (_parent: any, args: any, { prisma, userId }: Context) => {
+    // check if user exist
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    // throw error if user not found
+    if (!user) {
+      return {
+        message: "User Not Found!",
+        post: null,
+      };
+    }
+
+    const post = await prisma.post.findUnique({
+      where: {
+        id: Number(args?.postId),
+      },
+    });
+
+    // throw error if post not found
+    if (!post) {
+      return {
+        message: "Post Not Found!",
+        post: null,
+      };
+    }
+
+    // throw error if user id and author id are not the same
+    if (Number(user?.id) !== Number(post?.authorId)) {
+      return {
+        message: "Unauthorized Access!",
+        post: null,
+      };
+    }
+
+    // update post logic
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: Number(args?.postId),
+      },
+      data: {
+        title: args?.post?.title || post?.title,
+        content: args?.post?.content || post?.content,
+        published: args?.published !== undefined ? args?.published : post?.published,
+      },
+    });
+
+    // throw error if post is not updated
+    if (!updatedPost) {
+      return {
+        message: "Failed to Update Post!",
+        post: null,
+      };
+    }
+
+    // success response
+    return {
+      message: "Post Updated!",
+      post: updatedPost,
+    };
   },
 };
