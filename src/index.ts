6f8e4c301abf643b9be972e6b39bd9c62b8cdbb1
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-30 12:35:25 +0600

    feat: start server with express middlewere and deploy application

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+// import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers";
 import { Prisma, PrismaClient } from "@prisma/client";
 import { DefaultArgs } from "@prisma/client/runtime/library";
 import { getUserInfoFromToken } from "./utils";
+import express from "express";
+import { expressMiddleware } from "@apollo/server/express4";
+import { json } from "body-parser";
 
 export const prisma = new PrismaClient();
 
@@ -13,27 +16,64 @@ export type Context = {
   userId?: number;
 };
 
-const main = async () => {
-  const server = new ApolloServer({
+// const main = async () => {
+
+//   const { url } = await startStandaloneServer(server, {
+//     listen: { port: 4000 },
+//     context: async ({ req }): Promise<Context> => {
+//       const user = await getUserInfoFromToken(
+//         req.headers.authorization as string
+//       );
+
+//       return {
+//         prisma,
+//         userId: user?.userId,
+//       };
+//     },
+//   });
+
+//   console.log(`🚀  Server ready at: ${url}`);
+// };
+
+//  main();
+
+// Set up the Express app
+const app = express();
+
+async function main() {
+  // Initialize the Apollo Server
+  const server = new ApolloServer<Context>({
     typeDefs,
     resolvers,
   });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: async ({ req }): Promise<Context> => {
-      const user = await getUserInfoFromToken(
-        req.headers.authorization as string
+  // Await server startup
+  await server.start();
+
+  // Apply middleware to handle requests
+  app.use(
+    "/api/graphql",
+    json(),
+    expressMiddleware(server, {
+      context: async ({ req }): Promise<Context> => {
+        const user = await getUserInfoFromToken(
+          req.headers.authorization || ""
+        );
+        return { prisma, userId: user?.userId };
+      },
+    })
+  );
+
+  if (process.env.NODE_ENV !== "production") {
+    const PORT = 4000;
+    app.listen(PORT, () => {
+      console.log(
+        `🚀 Server is running on http://localhost:${PORT}/api/graphql`
       );
-
-      return {
-        prisma,
-        userId: user?.userId,
-      };
-    },
-  });
-
-  console.log(`🚀  Server ready at: ${url}`);
-};
+    });
+  }
+}
 
 main();
+
+export default app;
